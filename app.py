5c271cd7e320effb550774291ad4a3281be0ac6c
lorenzogaschino <lorenzo.gaschino@gmail.com>
2026-03-15 08:31:49 +0100

    Update app.py

--- app.py
+++ app.py
@@ -6,17 +6,20 @@ import time
 import os
 
 # --- CONFIGURAZIONE ---
+# Impostato layout centered per ottimizzazione mobile
 st.set_page_config(page_title="Family Booking", page_icon="🏠", layout="centered")
 
 st.markdown("""
     <head>
         <link rel="apple-touch-icon" href="https://raw.githubusercontent.com/lorenzogaschino/app-casa-vacanze/main/icona_app.png">
         <link rel="icon" href="https://raw.githubusercontent.com/lorenzogaschino/app-casa-vacanze/main/icona_app.png">
+        
         <meta name="apple-mobile-web-app-capable" content="yes">
         <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent">
         <meta name="apple-mobile-web-app-title" content="Family Booking">
     </head>
     <style>
+    /* Pulsanti grandi per uso touch */
     div.stButton > button { 
         width: 100% !important; 
         height: 4.5em !important; 
@@ -24,21 +27,10 @@ st.markdown("""
         font-weight: bold !important; 
         font-size: 1.2rem !important;
     }
+    /* Stili per il calendario */
     .cal-table { width:100%; border-collapse: collapse; table-layout: fixed; margin-bottom: 20px; }
     .cal-td { text-align:center; border:1px solid #eee; height:45px; position:relative; vertical-align: middle; }
     .day-num { position: absolute; top: 2px; left: 4px; font-size: 10px; color: #999; }
-    </style>
-    """, unsafe_allow_html=True)
-
-# --- FUNZIONI CORE ---
-def get_data():
-  
-    /* 3. Configurazione Mobile App (Meta Tags) */
-    @media icon {
-        <link rel="apple-touch-icon" href="icona_app.png">
-        <meta name="apple-mobile-web-app-capable" content="yes">
-        <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent">
-    }
     </style>
     """, unsafe_allow_html=True)
 
